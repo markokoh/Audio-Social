9eb821bd5b4169362ca73fb3a80a32229d34d82c
Mark Okoh <markokoh@yahoo.co.uk>
2023-02-06 15:52:34 -0800

    Created 'Feed.vue', and 'voicemailTranscriptions' firestore ref. Feed listens for updates on voicemailTranscriptions, and updates page in real time.

--- functions/index.js
+++ functions/index.js
@@ -1,84 +1,29 @@
-const admin = require("firebase-admin");
-admin.initializeApp();
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+initializeApp();
+
+const db = getFirestore();
 const functions = require("firebase-functions");
-const fetch = require("node-fetch");
-const {Messaging} = require("@signalwire/realtime-api");
-const fromNumber = process.env.FROM_NUMBER;
 
-const credentials = {
-  project: process.env.PROJECT_ID,
-  token: process.env.API_TOKEN,
-};
-
-
-const sendTextMessage = async (body, toNumber, media) => {
+exports.listTranscriptions = functions.https.onRequest(async (req, res) => {
   try {
-    const client = new Messaging.Client(credentials);
-    await client.send({
-      from: fromNumber,
-      to: toNumber,
-      body: body,
-      media: media,
+    console.log("CallSid", req.body.CallSid);
+    console.log("TranscriptionSid", req.body.TranscriptionSid);
+    console.log("TranscriptionText", req.body.TranscriptionText);
+
+    await db.collection("voicemailTranscriptions").doc(req.body.CallSid).set({
+
+      transcriptText: req.body.TranscriptionText,
+      transcriptionSid: req.body.TranscriptionSid,
+      recordingSid: req.body.RecordingSid,
+      callSid: req.body.CallSid,
+      from: req.body.From,
+      time: Date.now(),
+
     });
   } catch (err) {
-    console.log("Error sending text: ", err);
+    console.log("Error getting transactions: ", err);
   }
-};
-
-exports.getOpenAiResponse = functions.https.onRequest(async (req, res) => {
-  const messageBody = req.body.Body;
-  const userPhoneNo = req.body.From;
-
-  console.log("listenForOpenAi function");
-  const response = await fetch("https://api.openai.com/v1/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization":
-          "Bearer " + process.env.OPENAI_API_KEY,
-    },
-    body: JSON.stringify({
-      model: "text-davinci-003",
-      prompt: messageBody,
-      max_tokens: 70,
-      temperature: 0,
-    }),
-  });
-  const data = await response.json();
-  console.log(data.choices[0].text);
-  const textReply = data.choices[0].text;
-  sendTextMessage(textReply.replace("\n\n", ""), userPhoneNo, []);
-  res.end();
-});
-
-
-exports.createAiImage = functions.https.onRequest(async (req, res) => {
-  const messageBody = req.body.Body;
-  const userPhoneNo = req.body.From;
-
-  const response = await fetch("https://api.openai.com/v1/images/generations", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization":
-          "Bearer " + process.env.OPENAI_API_KEY,
-    },
-    body: JSON.stringify({
-      prompt: messageBody,
-      n: 1,
-      size: "512x512",
-    }),
-  });
-
-  const data = await response.json();
-  // "data" in the returned response object, is an array of image urls.
-  // Use the array index to access one, or forEach method
-  // to access all (if more than one is requested)
-  console.log("data.data[0].url", data.data[0].url);
-  const image1 = data.data[0].url;
-
-  sendTextMessage("", userPhoneNo, image1);
-
   res.end();
 });
 
