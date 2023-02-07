8acdd8611d2145f2ad10ffe9d183c5d48eae0273
Mark Okoh <markokoh@yahoo.co.uk>
2023-02-07 00:21:19 -0800

    FadeIn on feed now workiing.

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -4,6 +4,28 @@ import { ref } from "vue";
 import { db } from "../firebase/config";
 
 const messageArray = ref([]);
+const showText = ref(true);
+const hideText = () => {
+  if (showText.value === true) {
+    showText.value = false;
+  } else {
+    showText.value = true;
+  }
+};
+
+const makeHumanReadableTime = (epochMilliseconds) => {
+  const date = new Date(epochMilliseconds);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  const humanReadableTime = date.toLocaleDateString("en-US", options);
+
+  return humanReadableTime;
+};
 
 const q = query(
   collection(db, "voicemailTranscriptions"),
@@ -14,16 +36,90 @@ onSnapshot(q, (snapshot) => {
     if (change.type === "added") {
       console.log("New message: ", change.doc.data());
       messageArray.value.unshift(change.doc.data());
+      makeHumanReadableTime(change.doc.data().time);
     }
   });
 });
 </script>
 
 <template>
-  <h1>message feed 2</h1>
-  <div v-for="message in messageArray">
-    {{ message.transcriptText }}
+  <div style="margin-left: 25%">
+    <h1 class="font">Messages</h1>
+    <button
+      :class="{ 'audio-only': showText, 'text-only': !showText }"
+      @click="hideText"
+    >
+      text / audio
+    </button>
+  </div>
+  <div
+    v-for="(message, i) in messageArray"
+    :key="message.callSid"
+    :class="{ 'animate__animated animate__bounceInDown': i === 0 }"
+  >
+    <div class="message">
+      <div v-if="showText">{{ message.transcriptText }}</div>
+      <audio
+        style="border: none"
+        v-else
+        controls
+        :src="message.audioURL"
+      ></audio>
+    </div>
+    <div
+      style="margin-left: 25%; font-size: small; font-weight: bold"
+      class="font"
+    >
+      {{ makeHumanReadableTime(message.time) }}
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.message {
+  padding: 20px;
+  margin-top: 50px;
+  margin-bottom: 10px;
+  margin-left: 25%;
+  max-width: 50%;
+  border-radius: 5px;
+  background-color: gray;
+  color: white;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  /* font-size: small; */
+  min-height: 80px;
+  font-weight: bold;
+}
+
+button {
+  /* background-color: brown; */
+  color: white;
+  padding: 10px;
+  border-radius: 3px;
+  cursor: pointer;
+  border: none;
+  font-weight: bold;
+}
+
+audio::-webkit-media-controls-panel {
+  background-color: gray;
+}
+
+audio::-webkit-media-controls-play-button {
+  background-color: white;
+  border-radius: 50%;
+  color: white;
+}
+
+.audio-only {
+  background-color: blue;
+}
+
+.text-only {
+  background-color: red;
+}
+
+.font {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+</style>
