5e41a270253b607fe91e2c58490234391ba57f85
Mark Okoh <markokoh@yahoo.co.uk>
2023-02-06 22:11:40 -0800

    Added time to message. Feed working well.

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+const {getFirestore, Timestamp} = require("firebase-admin/firestore");
 initializeApp();
+// const createdAt = admin.firestore.Timestamp.fromDate(new Date());
 
 const db = getFirestore();
 const functions = require("firebase-functions");
@@ -10,6 +11,9 @@ exports.listTranscriptions = functions.https.onRequest(async (req, res) => {
     console.log("CallSid", req.body.CallSid);
     console.log("TranscriptionSid", req.body.TranscriptionSid);
     console.log("TranscriptionText", req.body.TranscriptionText);
+    const audioURL =
+        "https://onyourfrequency.signalwire.com/api/laml/2010-04-01/Accounts/5e5451d0-ffab-4b99-8758-9d6f135b4938/Recordings/" +
+        req.body.RecordingSid;
 
     await db.collection("voicemailTranscriptions").doc(req.body.CallSid).set({
 
@@ -19,6 +23,8 @@ exports.listTranscriptions = functions.https.onRequest(async (req, res) => {
       callSid: req.body.CallSid,
       from: req.body.From,
       time: Date.now(),
+      timestamp: Timestamp.now(),
+      audioURL,
 
     });
   } catch (err) {
